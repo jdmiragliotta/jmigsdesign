ba1d56554687fafc335daecb2425185f03125faa
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-02-16 15:00:40 -0500

    Corrected navbar scroll effects

---
+++ js/customscrollmagic.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+
+
+//   var controller = new ScrollMagic.Controller();
+
+//   // Scene Handler
+//   var scene1 = new ScrollMagic.Scene({
+//     triggerElement: "#scene", // point of execution
+    
+//     triggerHook: -2, // don't trigger until #pinned-trigger1 hits the top of the viewport
+//     reverse: true // allows the effect to trigger when scrolled in the reverse direction
+//   })
+//   .setPin("#nav") // the element we want to pin
+//   .addTo(controller);
+
+
+// })
--- js/main.js
+++ js/main.js
@@ -1,53 +1,32 @@
 $(document).ready(function(){
-  $(".slider").slider({
+  $('.slider').slider({
     full_width: true,
     height: 500,
     indicators: false
   });
 
   // ADDS ANIMATION TO NAVBAR LINKS
-  $(".nav-link").mouseenter(function(){
-    $(this).children().filter(":not(:animated)").animate({bottom: "5px"});
+  $('.nav-link').mouseenter(function(){
+    $(this).children().filter(':not(:animated)').animate({bottom: '5px'});
     });
-  $(".nav-link").mouseleave(function(){
-    $(this).children().animate({bottom: "0px"});
+  $('.nav-link').mouseleave(function(){
+    $(this).children().animate({bottom: '0px'});
     });
 
   // CREATES STICKY NAVBAR
   $(window).scroll(function() {
-    if( $(this).scrollTop() > $("header").height()-105) {
-      $("nav").addClass("scroll");
+    if( $(this).scrollTop() > $('header').height()-105) {
+      $('nav').addClass('scroll');
     } else {
-      $("nav").removeClass("scroll");
+      $('nav').removeClass('scroll');
     }
-    if( $(this).scrollTop() > $("header").height()-157) {
-      $("nav").addClass("scroll-color");
+    if( $(this).scrollTop() > $('header').height()-140) {
+      $('nav').addClass('scroll-color');
     } else {
-      $("nav").removeClass("scroll-color");
+      $('nav').removeClass('scroll-color');
     }
   });
-
-  // MINIMIZES TOP BAR
-  // $(window).scroll(function(){
-  //   var scroll = $(window).scrollTop();
-  //   if (scroll > 362 ) {
-  //     $(".logo-container").addClass("mini-header-scroll");
-  //   }
-  //   if (scroll <= 362 ) {
-  //     $(".logo-container").removeClass("mini-header-scroll");
-  //  }
-
-  // $(window).scroll(function() {
-  //   var scrollDis = $(this).scrollTop();
-  //   var currentPos = $('#nav-name').scrollTop();
-  //   if(scrollDis > 362) {
-  //     $('#nav-name').css('top', 120 - scrollDis);
-  //     $('#nav-name').css('font-size', 30 - (scrollDis/6));
-  //   } else if(scrollDis > 160) {
-  //     $('#nav-name').css('top', -22);
-  //   }
-  // });
-
+  // MINIMIZED LOGO AND SOCIAL LINKS
   $(window).scroll(function() {
   if ($(document).scrollTop() > 362) {
     $('.mini-header').addClass('shrink');
@@ -56,7 +35,6 @@ $(document).ready(function(){
   }
 });
 
-  $(".scrollspy").scrollSpy();
 
 });
   
