8a563f9b659e0ed333914177f42766402583a721
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-02-23 21:06:13 -0500

    Added development slider and styled

--- js/development.js
+++ js/development.js
@@ -1,31 +1,55 @@
-$(function () { // wait for document ready
-    // init
-    var controller = new ScrollMagic.Controller();
+$(document).ready(function(){
+  //See Options
+  var speed = 1000;              // fade Speed
+  var autoSwitch = true;        // auto Slider Option
+  var autoSwitchSpeed = 6000;   // Auto Slider Speed
 
-    // define movement of panels
-    var wipeAnimation = new TimelineMax()
-      // animate to second panel
-      .to("#slideContainer", 0.5, {z: -150})    // move back in 3D space
-      .to("#slideContainer", 1,   {x: "-25%"})  // move in to first panel
-      .to("#slideContainer", 0.5, {z: 0})       // move back to origin in 3D space
-      // animate to third panel
-      .to("#slideContainer", 0.5, {z: -150, delay: 1})
-      .to("#slideContainer", 1,   {x: "-50%"})
-      .to("#slideContainer", 0.5, {z: 0})
-      // animate to forth panel
-      .to("#slideContainer", 0.5, {z: -150, delay: 1})
-      .to("#slideContainer", 1,   {x: "-75%"})
-      .to("#slideContainer", 0.5, {z: 0});
+  // Add initial active class
+  $(".slide").first().addClass("active");
 
-    // create scene to pin and link animation
-    var scene = new ScrollMagic.Scene({
-        triggerElement: "#pinContainer",
-        triggerHook: "onLeave",
-        duration: "500%",
-        offset: -150
-      })
-      .setPin("#pinContainer")
-      .setTween(wipeAnimation)
-      .addIndicators() // add indicators (requires plugin)
-      .addTo(controller);
-  });
+  // Hide all slides
+  $(".slide").hide();
+
+  // Show first slide
+  $(".active").show();
+
+  // Next Handler
+  $("#next").on("click", nextSlide);
+
+  // Prev Handler
+  $("#prev").on("click", prevSlide);
+
+  // Auto Slider Handler
+  if(autoSwitch === true){
+    setInterval(nextSlide, autoSwitchSpeed);
+  }
+
+  // Switch to next slide
+  function nextSlide(){
+    $(".active").removeClass("active").addClass("oldActive");
+    if($(".oldActive").is(":last-child")){
+      $(".slide").first().addClass("active");
+    }else{
+      $(".oldActive").next().addClass("active");
+    }
+    $(".oldActive").removeClass("oldActive");
+    $(".slide").fadeOut(speed);
+    $(".active").fadeIn(speed);
+  };
+
+  // Switch to prev slide
+  function prevSlide(){
+    $(".active").removeClass("active").addClass("oldActive");
+    if($(".oldActive").is(":first-child")){
+      $(".slide").last().addClass("active");
+    }else{
+      $(".oldActive").prev().addClass("active");
+    }
+    $(".oldActive").removeClass("oldActive");
+    $(".slide").fadeOut(speed);
+    $(".active").fadeIn(speed);
+  };
+
+
+
+}); //End document.ready
