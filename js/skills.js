e2d2deafaa7d9cfb4ae6c375b3e10de6ebd5a59a
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-03-16 17:49:13 -0400

    Added fullstack icons

--- js/skills.js
+++ js/skills.js
@@ -7,7 +7,9 @@ $(document).ready(function(){
   $(".dev-skill").hide();
   $("#full-p").hide();
   $(".full").hide();
-  $(".skill-icon-mobile").hide();
+  $(".front-icon-mobile").hide();
+  $(".back-icon-mobile").hide();
+  $(".full-icon-mobile").hide();
 
   // Roll Over Events
   $("#design-skill").on("mouseover", function(){
@@ -42,8 +44,16 @@ $(document).ready(function(){
     $(".full").show().addClass("animated fadeInLeft");
   });
 
-   $(".skill-design-mobile").on("click", function(){
-    $(".skill-icon-mobile").show().addClass("animated fadeInLeft");
+   $(".front-end-mobile").on("click", function(){
+    $(".front-icon-mobile").show().addClass("animated fadeInLeft");
+  });
+
+  $(".back-end-mobile").on("click", function(){
+    $(".back-icon-mobile").show().addClass("animated fadeInLeft");
+  });
+
+  $(".full-stack-mobile").on("click", function(){
+    $(".full-icon-mobile").show().addClass("animated fadeInLeft");
   });
 
 
